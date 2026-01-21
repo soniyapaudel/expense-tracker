462ea238f96ae4c1eea75bf4f184eea346fe5899
Soniya <sunnapaudel@gmail.com>
2026-01-21 16:20:33 +0545

    Add CLI Expense Tracker with file persistence Features included:- Add new expenses with amount, category, and description - View all expenses in a formatted list - View total of all expenses - Filter expenses by category - Save expenses to file (data/expenses.txt) for persistence across sessions - Load expenses from file on startup - Automatic creation of data folder if missing - Input handling for CLI menu options

---
+++ README.md
@@ -0,0 +1,31 @@
+# Expense Tracker (CLI)
+A simple CLI-based Expense Tracker application built using Java and Maven 
+This project helps user track daily expenses, view totals, and filter expenses by category.
+
+
+# Features 
+-> Add new Expense 
+-> View all Expense 
+-> Calculate Total Expense 
+-> Filter expenses by Category 
+-> Clean Maven project Structure 
+
+# Tech Stack 
+-> Java
+-> MAven 
+-> CLI(Scanner-based input )
+
+# Project Structure 
+expense-tracker 
+-> pom.xml
+-> src
+----> main 
+-------> java
+----------> com.soniya.expensetracker 
+-----------> Main.java
+------------>repository 
+------------->service
+-------------> utl
+--------------> cli 
+-------------------> model 
+-----------------------> Expense.java 
--- src/main/java/com/soniya/expensetracker/cli/ExpenseCLI.java
+++ src/main/java/com/soniya/expensetracker/cli/ExpenseCLI.java
@@ -4,10 +4,22 @@ import com.soniya.expensetracker.model.Expense;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class ExpenseCLI {
     private final Scanner scanner = new Scanner(System.in);
     private List<Expense> expenses = new ArrayList<>();
+    private final String DATA_FOLDER = "data";
+    private final String FILE_NAME = DATA_FOLDER + "/expenses.txt";
+
+    public ExpenseCLI() {
+        loadExpenseFromFile();
+    }
 
     public void start() {
         while (true) {
@@ -24,28 +36,16 @@ public class ExpenseCLI {
 
             switch (choice) {
                 case 1:
-                    System.out.println("Enter amount: ");
-                    double amount = scanner.nextDouble();
-
-                    System.out.println("Enter Category: ");
-                    String category = scanner.nextLine();
-
-                    System.out.println("Enter description: ");
-                    String description = scanner.nextLine();
-
-                    Expense expense = new Expense(amount, category, description);
-                    expenses.add(expense);
-
-                    System.out.println("Expense added succesfully!");
+                    addExpense();
                     break;
                 case 2:
-                    System.out.println("View Expense chosen");
+                    viewExpenses();
                     break;
                 case 3:
-                    System.out.println("View Total chosen");
+                    viewTotal();
                     break;
                 case 4:
-                    System.out.println("Filter by Category chosen");
+                    filterByCategory();
                     break;
                 case 0:
                     System.out.println("Exiting...");
@@ -56,6 +56,104 @@ public class ExpenseCLI {
 
             }
 
+        }
+    }
+
+    private void addExpense() {
+        System.out.println("Enter amount: ");
+        double amount = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.println("Enter Category: ");
+        String category = scanner.nextLine();
+
+        System.out.println("Enter description: ");
+        String description = scanner.nextLine();
+
+        Expense expense = new Expense(amount, category, description);
+        expenses.add(expense);
+        saveExpenseToFile(expense);
+
+        System.out.println("Expense added succesfully!");
+    }
+
+    public void viewExpenses() {
+        if (expenses.isEmpty()) {
+            System.out.println("No expenses recorded.");
+            return;
+        }
+        System.out.println("\n List of Expenses ");
+        for (int i = 0; i < expenses.size(); i++) {
+            Expense e = expenses.get(i);
+            System.out.printf("%d. Amount: %2f, Category: %s, Description: %s%n", i + 1, e.getAmount(),
+                    e.getCategory(), e.getDescription());
+        }
+    }
+
+    private void viewTotal() {
+        double total = 0;
+        for (Expense e : expenses) {
+            total += e.getAmount();
+        }
+        System.out.printf("Total Expense: %.2f%n", total);
+    }
+
+    private void filterByCategory() {
+        System.out.println("Enter Category: ");
+        String cat = scanner.nextLine();
+
+        boolean found = false;
+        int count = 1;
+        for (Expense e : expenses) {
+            if (e.getCategory().equalsIgnoreCase(cat)) {
+                System.out.printf("%d. Amount: %2f, Category: %s, Description: %s%n", count++, e.getAmount(),
+                        e.getCategory(), e.getDescription());
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No expenses found for category: " + cat);
+        }
+    }
+
+    private void saveExpenseToFile(Expense e) {
+        try {
+            // create folder if missing for storing the data that user input
+
+            File folder = new File(DATA_FOLDER);
+            if (!folder.exists())
+                folder.mkdir();
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME, true))) {
+                writer.write(e.getAmount() + "," + e.getCategory() + "," + e.getDescription());
+                writer.newLine();
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Error saving expense to file:" + ex.getMessage());
+        }
+    }
+
+    private void loadExpenseFromFile() {
+        File file = new File(FILE_NAME);
+        if (!file.exists())
+            return;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",", 3);
+
+                if (parts.length == 3) {
+                    double amount = Double.parseDouble(parts[0]);
+                    String category = parts[1];
+                    String description = parts[2];
+                    expenses.add(new Expense(amount, category, description));
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("Error loading expenses from file: " + ex.getMessage());
         }
     }
 }
